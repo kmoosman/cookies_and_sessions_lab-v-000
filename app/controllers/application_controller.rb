29dbec689823364f22fc0d4e2082cff25cfb6a33
Katie Moosman <moosman.katie@gmail.com>
2019-04-23 23:16:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_cart
 
-  def cart
+  def current_cart
    session[:cart] ||= []
   end
 end
