8fae625ec276c4969b15740cc38424477436854b
Katie Moosman <moosman.katie@gmail.com>
2019-04-23 22:41:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def cart
-    if session[:cart] 
+    if session[:cart]
       cart = session[:cart]
     else
       session[:cart] = []
