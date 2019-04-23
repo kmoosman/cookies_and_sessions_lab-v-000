a0c05240c88330235cb86185bcdb318d52336aef
Katie Moosman <moosman.katie@gmail.com>
2019-04-23 23:16:54 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @cart = cart
+    @cart = current_cart
   end
 
    def add
-    cart << params[:product]
+    current_cart << params[:product]
     redirect_to root_path
   end
 end
